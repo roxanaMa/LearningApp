35a9cfff0727a88c2de7c5e6f510994083f23bba
roxanaMa <daniela.mardare@gmail.com>
2021-04-20 18:20:35 +0300

    - rename lesson description property in Content Model to codeText - add TestView - add NavigationView in Home View for tests - add properties in Content Model to track Test Selected - add method to begin test in content model

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -20,11 +20,16 @@ class ContentModel: ObservableObject {
     @Published var currentLesson: Lesson?
     var currentLessonIndex = 0
     
+    //Current question
+    @Published var currentQuestion: Question?
+    var currentQuestionIndex = 0
+    
     //Current lesson explanation
-    @Published var lessonDescription = NSAttributedString()
+    @Published var codeText = NSAttributedString()
     
     //Current selected content and test
     @Published var currentContentSelected:Int?
+    @Published var currentTestSelected:Int?
     
     var styleData: Data?
     
@@ -95,7 +100,7 @@ class ContentModel: ObservableObject {
         }
         //Set the current lesson
         currentLesson = currentModule!.content.lessons[currentLessonIndex]
-        lessonDescription = addStyling(currentLesson!.explanation)
+        codeText = addStyling(currentLesson!.explanation)
     }
     
     func nextLesson (){
@@ -108,7 +113,7 @@ class ContentModel: ObservableObject {
             
             //Set the current lesson property
             currentLesson = currentModule!.content.lessons[currentLessonIndex]
-            lessonDescription = addStyling(currentLesson!.explanation)        }
+            codeText = addStyling(currentLesson!.explanation)        }
         else {
             
             //Reset the lesson state
@@ -122,6 +127,24 @@ class ContentModel: ObservableObject {
         return (currentLessonIndex + 1 < currentModule!.content.lessons.count)
     }
    
+    func beginTest (_ moduleId:Int){
+        
+        //Set the current module
+        beginModule(moduleId)
+        
+        //Set the current question
+        currentQuestionIndex = 0
+        
+        //If there are questions, set the current question to the first one
+        if currentModule?.test.questions.count ?? 0 > 0 {
+            
+            currentQuestion = currentModule!.test.questions[currentQuestionIndex]
+            
+            //Set the question content
+            codeText = addStyling(currentQuestion!.content)
+        }
+    }
+    
     //MARK: Code styling
     private func addStyling(_ htmlString:String) -> NSAttributedString {
         
---
+++ LearningApp/Views/TestView.swift
@@ -0,0 +1,39 @@
+//
+//  TestView.swift
+//  LearningApp
+//
+//  Created by Roxy Mardare on 20.04.2021.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    
+    @EnvironmentObject var model: ContentModel
+    
+    var body: some View {
+        
+        if model.$currentQuestion != nil{
+            
+            VStack{
+                
+                //Question no
+                Text ("Question \(model.currentQuestionIndex + 1) of \(model.currentModule?.test.questions.count ?? 0)")
+                
+                //Question
+                CodeTextView()
+                
+                //Answers
+                
+                //Button
+            }
+            .navigationBarTitle("\(model.currentModule?.category ?? "") Test")
+        }
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
